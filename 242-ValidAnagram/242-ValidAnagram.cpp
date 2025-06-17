6fcf8a8f937f98c31015d311602cf3f533f9c2f4
Adarsh Kumar <136949560+AdarshKumarSr@users.noreply.github.com>
2025-06-17 12:01:57 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,14 @@
+// Last updated: 6/17/2025, 12:01:57 PM
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        if(s==t){
+            return true;
+        }
+        else return false;
+
+    }
+};
