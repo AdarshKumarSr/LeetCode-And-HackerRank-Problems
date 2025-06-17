ce04cd66e712618ba747a9d6b089d582d9fea723
Adarsh Kumar <136949560+AdarshKumarSr@users.noreply.github.com>
2025-06-17 08:58:57 +0530

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -0,0 +1,12 @@
+# Last updated: 6/17/2025, 8:58:56 AM
+class Solution:
+    def longestCommonPrefix(self, v: List[str]) -> str:
+        ans=""
+        v=sorted(v)
+        first=v[0]
+        last=v[-1]
+        for i in range(min(len(first),len(last))):
+            if(first[i]!=last[i]):
+                return ans
+            ans+=first[i]
+        return ans 
