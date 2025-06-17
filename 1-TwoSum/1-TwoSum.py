b4997a6b19c087d8ee27dcb7cf91a72a054258ef
Adarsh Kumar <136949560+AdarshKumarSr@users.noreply.github.com>
2025-06-17 08:59:13 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,11 @@
+# Last updated: 6/17/2025, 8:59:13 AM
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in seen:
+                return [seen[diff], i]
+            seen[num] = i
+        return []        
+        
