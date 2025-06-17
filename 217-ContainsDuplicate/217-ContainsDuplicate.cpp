5a3763a6ce1ceda44960c367c9f87cff28c57418
Adarsh Kumar <136949560+AdarshKumarSr@users.noreply.github.com>
2025-06-17 10:33:53 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -0,0 +1,13 @@
+// Last updated: 6/17/2025, 10:33:52 AM
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i=1; i<nums.size(); ++i){
+            if(nums[i] == nums[i -1]){
+                return true;
+            }
+        }
+            return false;
+    }
+};
