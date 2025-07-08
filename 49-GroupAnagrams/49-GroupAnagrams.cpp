ecd0268596e24fb887e72bc22434fd2be490b5d3
Adarsh Kumar <136949560+AdarshKumarSr@users.noreply.github.com>
2025-07-08 16:55:56 +0530

    File updated at 7/8/2025, 4:55:56 PM

--- 49-GroupAnagrams/49-GroupAnagrams.cpp
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -1,22 +1,22 @@
-// Last updated: 7/6/2025, 7:31:37 PM
+// Last updated: 7/8/2025, 4:55:56 PM
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // to store the anagam_map;
-        unordered_map<string, vector<string>> anagram_map;
+       unordered_map<string , vector<string>> anagram_map;
 
-        for(string& words : strs) {  // runs loop over input
-            string sorted = words;
-            sort(sorted.begin(), sorted.end());
-            anagram_map[sorted].push_back(words);
+       for(string& words : strs){
+        string sorted = words;
+        sort(sorted.begin(), sorted.end());
+        anagram_map[sorted].push_back(words);
+       }
+
+        vector<vector<string>> res;
+        for(auto& entry  : anagram_map ){
+            res.push_back(entry.second);
         }
 
-        vector<vector<string>> resl;
-        for(auto& entry : anagram_map){
-            resl.push_back(entry.second);
-        }
-
-        return resl;
+        return res;
         
     }
+
 };
