9229cc2a4c27449ced267583af88b2d5e26c1398
Adarsh Kumar <136949560+AdarshKumarSr@users.noreply.github.com>
2025-07-19 19:30:22 +0530

    Create 2813-ToBeOrNotToBe

---
+++ 2813-ToBeOrNotToBe/2813-ToBeOrNotToBe.js
@@ -0,0 +1,22 @@
+// Last updated: 7/19/2025, 7:30:22 PM
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe : function(expected){
+            if(val === expected) return true;
+            else throw new Error ("Not Equal");
+        },
+        notToBe : function(expected){
+            if(val !== expected) return true;
+            else throw new Error ("Equal");
+        }
+    };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
