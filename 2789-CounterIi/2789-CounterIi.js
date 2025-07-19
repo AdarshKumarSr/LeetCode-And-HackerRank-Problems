444e1d9bee97a66d533c5e3cc547f9ab53395eac
Adarsh Kumar <136949560+AdarshKumarSr@users.noreply.github.com>
2025-07-19 19:30:26 +0530

    Create 2789-CounterIi

---
+++ 2789-CounterIi/2789-CounterIi.js
@@ -0,0 +1,28 @@
+// Last updated: 7/19/2025, 7:30:26 PM
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let current = init;
+
+    return {
+        increment: function() {
+            return ++current;
+        },
+        reset: function() {
+            current = init;
+            return current;
+        },
+        decrement: function() {
+            return --current;
+        }
+    };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset();     // 5
+ * counter.decrement(); // 4
+ */
