a289a6d8c70c703dbb0a6c443ba28384071f8e68
Adarsh Kumar <136949560+AdarshKumarSr@users.noreply.github.com>
2025-08-01 03:34:12 +0530

    Create 898-BitwiseORsofSubarrays

---
+++ 898-BitwiseORsofSubarrays/898-BitwiseORsofSubarrays.java
@@ -0,0 +1,19 @@
+// Last updated: 8/1/2025, 3:34:12 AM
+class Solution {
+    public int subarrayBitwiseORs(int[] arr) {
+        Set<Integer> res = new HashSet<>();
+        Set<Integer> cur = new HashSet<>();
+
+        for (int num : arr) {
+            Set<Integer> next = new HashSet<>();
+            next.add(num);
+            for (int x : cur) {
+                next.add(x | num);
+            }
+            cur = next;
+            res.addAll(cur);
+        }
+
+        return res.size();
+    }
+}
